de2c3019b453b0b1279c4024498ba68441ca5500
sadekcric4 <sadekcric4@gmail.com>
2024-02-13 08:04:07 +0600

    all geometry added

--- script.js
+++ script.js
@@ -11,11 +11,32 @@ function multiplicationCalculate(cm1, cm2) {
 
 document.getElementById("triangleBtn").addEventListener("click", function () {
   const area = 0.5 * multiplicationCalculate(`base`, "height");
-  document.getElementById('triangleArea').innerText = area;
+  document.getElementById("triangleArea").innerText = area;
 });
 
-document.getElementById('rectangleBtn').addEventListener('click', function(){
-  
-  const area=multiplicationCalculate('width', 'length');
-  document.getElementById('rectangleArea').innerText=area;
-})
+document.getElementById("rectangleBtn").addEventListener("click", function () {
+  const area = multiplicationCalculate("width", "length");
+  document.getElementById("rectangleArea").innerText = area;
+});
+
+document.getElementById("parallelogram_btn").addEventListener("click", function () {
+  const area = multiplicationCalculate("parallelogram_base", "parallelogram_height");
+  document.getElementById("parallelogram_aria").innerText = area;
+});
+
+document.getElementById("rhombus-btn").addEventListener("click", function () {
+  const area =0.5* multiplicationCalculate("rhombus-d1", "rhombus-d2");
+  document.getElementById("rhombus-aria").innerText = area;
+});
+
+document.getElementById("pentagon_btn").addEventListener("click", function () {
+  const area =0.5* multiplicationCalculate("pentagon_p", "pentagon_b");
+  document.getElementById("pentagon_aria").innerText = area;
+});
+
+document.getElementById("ellipse_btn").addEventListener("click", function () {
+  const area = 0.1416 * multiplicationCalculate("ellipse_a", "ellipse_b");
+  document.getElementById("ellipse_aria").innerText = area;
+});
+
+
